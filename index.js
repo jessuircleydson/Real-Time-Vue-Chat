49d9bfbdd280aead5bf062fb18a81f4f85f32a1a
Jessuir Cleydson <jessuircleydson@gmail.com>
2021-06-05 18:52:48 -0300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit('entrou', 'Uma pessoa entrou no chat!');
+    socket.on('disconnect', () => {
+        io.emit('saiu', 'Uma pessoa saiu do chat!');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
